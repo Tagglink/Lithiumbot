a92d1a8a8e6342026797da55cd2ba42304bfe6fd
simonbasken <simonbasken@users.noreply.github.com>
2017-02-27 11:12:53 +0100

    Lennygene
    (   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
     (   ͡° ͜ʖ ͡° )  ͡° ͜ʖ ͡° )
      (   ͡° ͜ʖ ͡° )  ͜ʖ ͡° )
       (   ͡° ͜ʖ ͡° )  ͡° )
        (   ͡° ͜ʖ ͡° )° )
         (   ͡° ͜ʖ ͡° )
        ( (   ͡° ͜ʖ ͡° )
       (   (   ͡° ͜ʖ ͡° )
      (   ͡° (   ͡° ͜ʖ ͡° )
     (   ͡° ͜ʖ (   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° (   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° ) (   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
     (   ͡° ͜ʖ ͡° )  ͡° ͜ʖ ͡° )
      (   ͡° ͜ʖ ͡° )  ͜ʖ ͡° )
       (   ͡° ͜ʖ ͡° )  ͡° )
        (   ͡° ͜ʖ ͡° )° )
         (   ͡° ͜ʖ ͡° ))
         (   ͡° ͜ʖ ͡° )
        ( (   ͡° ͜ʖ ͡° )
       (   (   ͡° ͜ʖ ͡° )
      (   ͡° (   ͡° ͜ʖ ͡° )
     (   ͡° ͜ʖ (   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° (   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
    (   ͡° ͜ʖ ͡° ) (   ͡° ͜ʖ ͡° )

--- src/Modules/Copypastas.cs
+++ src/Modules/Copypastas.cs
@@ -281,7 +281,34 @@ The dog absorbs the memes",
             @"Life is like a cabbage: sometimes it is green and round, and sometimes mom wish you were never born",
             @"If there is a fire in your house make sure you save all the cabbages before you even think about finding your children",
             @"Sometimes you hate your life and dont want to be alive anymore but then you think about cabbage and know that everything will be ok",
-            @"If you push a cabbage under water he will alway float right back up to the top because he miss you so much"
+            @"If you push a cabbage under water he will alway float right back up to the top because he miss you so much",
+            @"(   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
+ (   ͡° ͜ʖ ͡° )  ͡° ͜ʖ ͡° )
+  (   ͡° ͜ʖ ͡° )  ͜ʖ ͡° )
+   (   ͡° ͜ʖ ͡° )  ͡° )
+    (   ͡° ͜ʖ ͡° )° )
+     (   ͡° ͜ʖ ͡° )
+    ( (   ͡° ͜ʖ ͡° )
+   (   (   ͡° ͜ʖ ͡° )
+  (   ͡° (   ͡° ͜ʖ ͡° )
+ (   ͡° ͜ʖ (   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° (   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° ) (   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
+ (   ͡° ͜ʖ ͡° )  ͡° ͜ʖ ͡° )
+  (   ͡° ͜ʖ ͡° )  ͜ʖ ͡° )
+   (   ͡° ͜ʖ ͡° )  ͡° )
+    (   ͡° ͜ʖ ͡° )° )
+     (   ͡° ͜ʖ ͡° ))
+     (   ͡° ͜ʖ ͡° )
+    ( (   ͡° ͜ʖ ͡° )
+   (   (   ͡° ͜ʖ ͡° )
+  (   ͡° (   ͡° ͜ʖ ͡° )
+ (   ͡° ͜ʖ (   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° (   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° )(   ͡° ͜ʖ ͡° )
+(   ͡° ͜ʖ ͡° ) (   ͡° ͜ʖ ͡° )"
         };
     }
 }
